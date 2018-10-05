0a77ded1f561d8a07e2e621b137f6799b5b5a099
Milena Ognyanova <mile0416@edu.easj.dk>
2018-10-05 11:24:41 +0200

    Add project files.

---
+++ DLLfile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DLLfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
